00d733f4b2223c7227d2d7d0beaa4c14f60845c9
wirawan <xnamine@gmail.com>
2019-09-09 08:37:11 +0700

    Initial Commit

---
+++ iCubeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCubeTest
+//
+//  Created by wirawan on 9/9/19.
+//  Copyright © 2019 wirawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
